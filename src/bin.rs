9df652e9766e1ddfe71579ed97e564d73542342a
Matthew Daly <matthewd673@outlook.com>
2025-05-16 21:07:33 -0700

    Add Wasm formula and solve API

--- src/bin.rs
+++ src/bin.rs
@@ -3,8 +3,10 @@ mod parser;
 mod solver;
 mod trail;
 
+use std::collections::HashSet;
 use std::env;
 use std::fs;
+use crate::cnf::Lit;
 
 fn main() {
     // Get filename
@@ -27,5 +29,33 @@ fn main() {
         },
     }
 
-    println!("{}", saguaro::solve_cnf(contents));
+    let mut cnf;
+    match parser::parse(contents) {
+        Some(f) => { cnf = f; },
+        None => {
+            println!("Syntax error in CNF");
+            std::process::exit(1);
+        }
+    }
+
+    let solution = solver::solve(&mut cnf);
+    println!("{}", fmt_solution(solution, cnf.num_vars()));
+}
+
+fn fmt_solution(result: Result<HashSet<Lit>, ()>, num_vars: usize) -> String {
+    match result {
+        Ok(assign) =>
+            format!("s SATISFIABLE\nv {}0",
+                    fmt_assignments(&assign, num_vars as i32)),
+        Err(()) => String::from("s UNSATISFIABLE"),
+    }
+}
+
+fn fmt_assignments(assignments: &HashSet<Lit>, num_vars: i32) -> String {
+    (1..num_vars + 1).fold(String::new(), |acc, var| {
+        let mul = if assignments.contains(&var) { 1 } else { -1 };
+        let lit = (var * mul).to_string();
+
+        format!("{}{} ", acc, lit)
+    })
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,46 +3,119 @@ mod cnf;
 mod solver;
 mod trail;
 
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use wasm_bindgen::prelude::*;
-use crate::cnf::Lit;
+use crate::cnf::{Cnf, Clause, Lit};
 
 #[wasm_bindgen]
-pub fn solve_cnf(str: String) -> String {
-    let mut cnf;
-    match parser::parse(str) {
-        Some(f) => { cnf = f; },
-        _ => { // TODO: Proper error handling
-            return String::from("Syntax error in CNF");
-        },
-    }
-
-    get_solution(solver::solve(&mut cnf), cnf.num_vars())
+pub struct Formula {
+    expr: And,
 }
 
-/**
- * Given a result, get a string representation of the solution. The output is
- * in the format expected by the SAT Competition:
- * https://satcompetition.github.io/2024/output.html
- */
-fn get_solution(result: Result<HashSet<Lit>, ()>, num_vars: usize) -> String {
-    match result {
-        Ok(assign) =>
-            format!("s SATISFIABLE\nv {}0",
-                    get_assignments_str(&assign, num_vars as i32)),
-        Err(()) => String::from("s UNSATISFIABLE"),
-    }
+#[wasm_bindgen]
+pub struct And {
+    clauses: Vec<Or>,
 }
 
-fn get_assignments_str(assignments: &HashSet<Lit>, num_vars: i32) -> String {
-    (1..num_vars + 1).fold(String::new(), |acc, var| {
-        let lit = if assignments.contains(&var) {
-            var.to_string()
+#[wasm_bindgen]
+pub struct Or {
+    lits: Vec<Literal>,
+}
+
+#[wasm_bindgen]
+pub struct Literal {
+    var: String,
+    negated: bool,
+}
+
+#[wasm_bindgen]
+pub fn formula(expr: And) -> Formula {
+    Formula { expr }
+}
+
+#[wasm_bindgen]
+pub fn and(clauses: Vec<Or>) -> And {
+    And { clauses }
+}
+
+#[wasm_bindgen]
+pub fn or(lits: Vec<Literal>) -> Or {
+    Or { lits }
+}
+
+#[wasm_bindgen]
+pub fn bool(name: String) -> Literal {
+    Literal { var: name, negated: false }
+}
+
+#[wasm_bindgen]
+pub fn not(lit: Literal) -> Literal {
+    Literal { var: lit.var, negated: !lit.negated }
+}
+
+#[wasm_bindgen]
+pub fn solve(formula: Formula) -> js_sys::Object {
+    let (mut cnf, var_map) = formula_to_cnf(&formula);
+    let solution = solver::solve(&mut cnf);
+    solution_to_js_obj(&solution, &var_map)
+}
+
+fn formula_to_cnf(formula: &Formula) -> (Cnf, HashMap<i32, String>) {
+    let mut var_to_ind_map = HashMap::new();
+    let mut ind_to_var_map = HashMap::new();
+    let clauses: Vec<Clause> = formula.expr.clauses.iter()
+        .map(|clause| clause.lits.iter()
+            .map(|lit| {
+                let mul = if lit.negated { -1 } else { 1 };
+                (match var_to_ind_map.get(&lit.var) {
+                    Some(&ind) => ind,
+                    None => {
+                        var_to_ind_map.insert(lit.var.clone(),
+                                              var_to_ind_map.len() + 1);
+                        ind_to_var_map.insert(var_to_ind_map.len() as i32,
+                                              lit.var.clone());
+                        var_to_ind_map.len()
+                    },
+                } as i32) * mul
+            })
+            .collect()
+        )
+        .collect();
+
+    (Cnf::new(clauses, var_to_ind_map.len()), ind_to_var_map)
+}
+
+fn solution_to_js_obj(solution: &Result<HashSet<Lit>, ()>,
+                      var_map: &HashMap<i32, String>) -> js_sys::Object {
+    let sat_key = JsValue::from("sat");
+    let assignments_key = JsValue::from("assignments");
+
+    let js_obj_map = js_sys::Map::new();
+    match solution {
+        Ok(assign) => {
+            let assign_js_map = assignments_to_js_map(&assign, var_map);
+
+            js_obj_map.set(&sat_key, &JsValue::TRUE);
+            js_obj_map.set(&assignments_key, &assign_js_map);
         }
-        else {
-            (-var).to_string()
-        };
+        Err(()) => {
+            js_obj_map.set(&sat_key, &JsValue::FALSE);
+        }
+    }
 
-        format!("{}{} ", acc, lit)
-    })
+    js_sys::Object::from_entries(&JsValue::from(js_obj_map)).unwrap()
+}
+
+fn assignments_to_js_map(assign: &HashSet<Lit>,
+                         var_map: &HashMap<i32, String>) -> js_sys::Map {
+    let assign_js_map = js_sys::Map::new();
+    (1..var_map.len() as i32 + 1).for_each(|var| {
+        let var_name = var_map.get(&var).unwrap();
+
+        let key = JsValue::from(var_name);
+        let value = JsValue::from(assign.contains(&var));
+        assign_js_map.set(&key, &value);
+    });
+
+    assign_js_map
 }
